2900035bc3ae12b4ee497a8886d84f81ab8a15f6
vladji <vladjislav@gmail.com>
2020-02-12 18:35:12 +0300

    build

---
+++ precache-manifest.f21c48ac4b4ef76f672f0bbc1e43dbff.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007dfb2d7ab5fb9024ecb7934aed4bf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c55950fae5bf6d418906",
+    "url": "/static/css/main.345d3873.chunk.css"
+  },
+  {
+    "revision": "a263c6f10bd9d557ae89",
+    "url": "/static/js/2.d63a547f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d63a547f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c55950fae5bf6d418906",
+    "url": "/static/js/main.1b89be63.chunk.js"
+  },
+  {
+    "revision": "d7eab5cb1f465faaf289",
+    "url": "/static/js/runtime-main.708445b0.js"
+  },
+  {
+    "revision": "165166d5c9e027367cb75b2ac89dcfc0",
+    "url": "/static/media/error.165166d5.mp3"
+  },
+  {
+    "revision": "90dcd99965c07277a17c72999e687535",
+    "url": "/static/media/game-theme-bckg.90dcd999.png"
+  },
+  {
+    "revision": "cfa2319ab32ee1dd54017726f59f934a",
+    "url": "/static/media/gave-over-img.cfa2319a.jpg"
+  },
+  {
+    "revision": "4f7014ff137f5b9ad9c624a52d4db6a5",
+    "url": "/static/media/intoxicated_gta.4f7014ff.mp3"
+  },
+  {
+    "revision": "32fb2d152289dc6bff339eca0288d0ad",
+    "url": "/static/media/pixel-logo.32fb2d15.ttf"
+  },
+  {
+    "revision": "e778eaa0770c9d9442352f4e30af7ea5",
+    "url": "/static/media/pixel-txt.e778eaa0.ttf"
+  },
+  {
+    "revision": "a1e9e8b671592af8b3a503b23bbba6eb",
+    "url": "/static/media/succes.a1e9e8b6.mp3"
+  }
+]);
